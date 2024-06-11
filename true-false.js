3fb9e05b26ad88e2268008781d88377e61a09254
anayethasan <mdneloy256@gmail.com>
2024-06-11 17:35:06 +0600

    full condition

---
+++ true-false.js
@@ -0,0 +1,15 @@
+// false all condition 
+// o
+// ""
+// null
+// NaN
+// if variable is undefine then also condition false
+
+
+const isThisTureOrFalse = 4;
+
+if (isThisTureOrFalse){
+    console.log("Condition is true");
+}else{
+    console.log("Condition is false");
+}
